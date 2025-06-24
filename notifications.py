229cc1d534de9be0ef27099be25d4ccf926b8e4d
DuoLE3383 <duole.hcm@gmail.com>
2025-06-24 10:52:16 +0700

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -161,13 +161,13 @@ class NotificationHandler:
             timeframe_escaped = TelegramHandler.escape_markdownv2(config.TIMEFRAME)
             caption_text = (
                 f"🚀 *AI Trading Bot Activated* 🚀\n\n"
-                f"The bot is now live and analyzing `{symbols_count}` pairs on the `{timeframe_escaped}` timeframe\\."
-                f"\n\n📡 Get ready for real-time market signals every 10 minutes!\n\n"
-                f"💰 *New to Binance? Get a \\$100 Bonus\\!*"
-                f"\nSign up and earn a *100 USD trading fee rebate voucher\\!*\n\n "
-                f"🔗 *Register Now:*\n"
-                f"https://www.binance.com/activity/referral-entry/CPA?ref=CPA_006MBW985P\n\n"
-                f"\\----------------------------------------\\\n"
+                f"The bot is now live and analyzing `{symbols_count}` pairs on the `{timeframe_escaped}` timeframe\\.\n\n"
+                f"📡 Get ready for real\\-time market signals every 10 minutes\\!\n\n"
+                f"💰 *New to Binance\\? Get a \\$100 Bonus\\!*\\n"
+                f"Sign up and earn a *100 USD trading fee rebate voucher\\!*\\n\n"
+                f"🔗 *Register Now\\:*\n"
+                f"https://www\\.binance\\.com/activity/referral\\-entry/CPA\\?ref\\=CPA\\_006MBW985P\n\n"
+                f"\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-\\-"
             )
             photo_url = "https://github.com/DuoLE3383/AI-trending/blob/main/100usd.png?raw=true"
 
