4753013bd52e57566ef31114f39bb00283949a16
DuoLE3383 <duole.hcm@gmail.com>
2025-06-29 09:09:36 +0700

    Update api_server.py

--- src/api_server.py
+++ src/api_server.py
@@ -3,7 +3,7 @@ import sqlite3
 import logging
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import config as config # Import config để lấy đường dẫn DB
+from . import config  # Import config module to access database path
 from .performance_analyzer import get_performance_stats # Import hàm tính toán đã có
 # --- Chạy Server ---
 app = Flask(__name__)
