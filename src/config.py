2a843c63a261a30571d1a74ffbef8eda66b7f64e
DuoLE3383 <duole.hcm@gmail.com>
2025-06-29 12:16:52 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ TELEGRAM_MESSAGE_THREAD_ID = os.getenv("TELEGRAM_MESSAGE_THREAD_ID") # ID của 
 # ==============================================================================
 # === 2. DATABASE
 # ==============================================================================
-SQLITE_DB_PATH = "trend_analysis.db"
+SQLITE_DB_PATH = "./src/trend_analysis.db"
 
 # ==============================================================================
 # === 3. SYMBOL & MARKET DATA SETTINGS
